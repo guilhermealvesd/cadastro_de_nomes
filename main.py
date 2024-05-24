95de8ebe5b7a906f75a1c20b33a29dba31af30bc
guilhermealvesd <alvessig@gmail.com>
2024-05-24 19:33:32 -0300

    Versão 1.0.

---
+++ main.py
@@ -0,0 +1,17 @@
+#Lista de Frutas
+
+frutas = []
+
+#Usuário informa o nome da nova fruta
+
+
+nova_fruta = input('Informe o nome da nova fruta: ')
+
+#Nova fruta é inserida na lista
+
+frutas.append(nova_fruta)
+
+#Exibe na tela a nova lista
+
+for fruta in frutas:
+    print(fruta)
